eaf269c03603ad8c72641653eba0e841c00afb00
konrad.czajka@gmail.com <konrad.czajka@gmail.com>
2025-05-12 13:56:25 +0200

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI4MDk2MA/spectral.js?branch_slug=master&token=ea47e0f4-aae7-401f-acaf-c71ecef1af30";
+export default { extends: ruleset };
---
+++ docs/taga/Some-article.md
@@ -0,0 +1,8 @@
+---
+stoplight-id: omgzyqtxm3nwa
+tags: [tagA]
+---
+
+# Some-article
+
+The beginning of an awesome article...
